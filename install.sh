59f9456c9d04ec448b1df106417339acda9c4c36
wu.nerd <wu.nerd@gmail.com>
2018-05-25 22:13:01 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+BASEDIR=$(cd `dirname $0` && pwd);
+[ ! -f ~/.vimrc ] || mv ~/.vimrc ~/.vimrc-`date +%s`;
+[ ! -f ~/.zshrc ] || mv ~/.zshrc ~/.zshrc-`date +%s`;
+[ ! -f ~/.tmux.conf ] || mv ~/.tmux.conf ~/.tmux.conf-`date +%s`;
+
+ln -s $BASEDIR/.vimrc ~/.vimrc;
+ln -s $BASEDIR/.tmux.conf ~/.tmux.conf;
+ln -s $BASEDIR/.zshrc ~/.zshrc;
