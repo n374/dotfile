a1e28ea779d4160384370b4ec09c2ad5e78d1d0e
wu.nerd <wu.nerd@gmail.com>
2021-10-24 21:54:55 +0800

    Add ideavimrc

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@ BASEDIR=$(cd `dirname $0` && pwd);
 [ ! -f ~/.vimrc ] || mv ~/.vimrc ~/.vimrc-`date +%s`;
 [ ! -f ~/.zshrc ] || mv ~/.zshrc ~/.zshrc-`date +%s`;
 [ ! -f ~/.tmux.conf ] || mv ~/.tmux.conf ~/.tmux.conf-`date +%s`;
+[ ! -f ~/.ideavimrc ] || mv ~/.ideavimrc ~/.ideavimrc-`date +%s`;
 
 ln -s $BASEDIR/.vimrc ~/.vimrc;
 ln -s $BASEDIR/.tmux.conf ~/.tmux.conf;
 ln -s $BASEDIR/.zshrc ~/.zshrc;
+ln -s $BASEDIR/.ideavimrc ~/.ideavimrc;
